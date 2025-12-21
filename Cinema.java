5d6b58ce62615698d0c2d3c9662b745f4f5492c8
ANAYSSA <48955674+ANAYSSA@users.noreply.github.com>
2025-12-22 00:48:49 +0500

    Update Cinema.java

--- Cinema.java
+++ Cinema.java
@@ -1,26 +1,46 @@
+import java.util.*;
+import java.util.stream.Collectors;
 public class Cinema {
     private String name;
     private int seats;
-
+    private List<Film> films = new ArrayList<>();
     public Cinema(String name, int seats) {
         this.name = name;
         this.seats = seats;
     }
-
-    public int getSeats() { return seats; }
-
+    public void addMovie(Film film) {
+        films.add(film);
+    }
+    public void findFilm(String title) {
+        for (Film f : films) {
+            if (f.getTitle().equalsIgnoreCase(title)) {
+                System.out.println("Найден: " + f);
+                return;
+            }
+        }
+        System.out.println("Film not found");
+    }
+    public void sortByPrice() {
+        films.sort(Comparator.comparingDouble(Film::getPrice));
+        System.out.println("List sorted по цене");
+    }
+    public void showCheapFilms(double maxPrice) {
+        System.out.println("Фильмы до " + maxPrice + ":");
+        for (Film f : films) {
+            if (f.getPrice() <= maxPrice) {
+                System.out.println(f);
+            }
+        }
+    }
     public void reserve(Viewer viewer, Film film) {
-        System.out.println(viewer.getName() + " tries to buy \"" + film.getTitle() + "\"");
-
-        if (seats <= 0) {
-            System.out.println("No seats left!");
-            return;
-        }
-        if (viewer.buyTicket(film)) {
+        if (seats > 0 && viewer.buyTicket(film)) {
             seats--;
-            System.out.println("Ticket bought! Balance now: " + viewer.getBalance());
+            System.out.println(viewer.getName() + " купил билет на фильм" + film.getTitle());
         } else {
-            System.out.println("Not enough money.");
+            System.out.println("Ошибка бронирования для " + viewer.getName());
         }
+    }
+    public void showAllFilms() {
+        films.forEach(System.out::println);
     }
 }
