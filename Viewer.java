1d8a17e4616ce01ad723b997c1522c4319e18038
ANAYSSA <48955674+ANAYSSA@users.noreply.github.com>
2025-12-22 00:49:20 +0500

    Implement toString method in Viewer class
    Added toString method for better representation.

--- Viewer.java
+++ Viewer.java
@@ -1,22 +1,21 @@
 public class Viewer {
     private String name;
     private double balance;
-
     public Viewer(String name, double balance) {
         this.name = name;
         this.balance = balance;
     }
-
     public String getName() { return name; }
     public double getBalance() { return balance; }
-
-    public void setBalance(double balance) { this.balance = balance; }
-
     public boolean buyTicket(Film film) {
         if (balance >= film.getPrice()) {
             balance -= film.getPrice();
             return true;
         }
         return false;
+    }
+    @Override
+    public String toString() {
+        return "Зритель: " + name + ", Баланс: " + balance;
     }
 }
